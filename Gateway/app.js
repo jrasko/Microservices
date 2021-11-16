b2e72211ff87aa1c05415fe758e8621e226f2bc8
jrasko <j.raskob@tarent.de>
2021-11-16 21:04:09 +0100

    Added metrics explicit metrics port

--- Gateway/app.js
+++ Gateway/app.js
@@ -1,5 +1,17 @@
+const express = require('express');
 import { ApolloServer } from 'apollo-server'
 import gateway from './createSubs.js'
+const client = require('prom-client')
+client.collectDefaultMetrics()
+
+const app = express();
+app.get('/metrics', (req, res) => {
+    res.set('Content-Type', client.register.contentType)
+    client.register.metrics().then((str) => {
+            res.end(str)
+        }
+    )
+})
 
 const runServer = async () => {
     // Get newly merged schema
@@ -9,6 +21,10 @@ const runServer = async () => {
         console.log(`Running at ${url}`);
     });
 }
+app.listen({port: 5000}, () => {
+    console.log(`🚀 Metrics started`);
+});
+
 
 try {
     runServer().then(r => console.log(r));
